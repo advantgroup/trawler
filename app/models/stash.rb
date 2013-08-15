b16ff78c9314fb1a5cb1246a9e30244cecac3b94
txnash <txnash@gmail.com>
2013-08-16 09:29:50 +1000

    Better.

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -1,12 +1,21 @@
 class Stash < ActiveRecord::Base
   attr_accessible :name, :practice, :email, :phone, :body 
 
-  def self.fight(url)
-    target = Nokogiri::HTML(open(url)) do |config|
-      config.noblanks
-    end
-    targeted = target.css('#mainbottom span')
-    puts targeted
+  @url = 'http://www.fpa.com.au/default.asp?action=article&ID=22159&Member='
+
+  def self.scan(i, l)
+  	until i == l do
+	    target = Nokogiri::HTML(open(@url + i.to_s)) do |config|
+	      config.noblanks
+	    end
+	    if target.at_css('#ProfileDetails')
+	    	targeted = target.css('#ProfileDetails td')
+	    	puts targeted
+	    else
+	    	puts 'No profile found at record no. ' + i.to_s
+	    end
+	    i += 1
+	  end
   end
 
 end
