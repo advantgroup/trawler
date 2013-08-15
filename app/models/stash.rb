aca39622ca783388d925bfd5c16778a80f532479
Thomas Nash <txnash@gmail.com>
2013-08-15 16:57:12 +1000

    Kinda targeting.

--- app/models/stash.rb
+++ app/models/stash.rb
@@ -1,16 +1,12 @@
 class Stash < ActiveRecord::Base
   attr_accessible :name, :practice, :email, :phone, :body 
 
-  def self.fight
-    a = Mechanize.new { |agent|
-      agent.user_agent_alias = 'Mac Safari'
-    }
-    a.get('http://advant.com.au/') do |home_page|
-      target = home_page.elements_with(:id => 'mainbottom')
-      target.images.each do |link|
-        puts images.url
-      end
+  def self.fight(url)
+    target = Nokogiri::HTML(open(url)) do |config|
+      config.noblanks
     end
+    targeted = target.css('#mainbottom span')
+    puts targeted
   end
 
 end
